af0a14ebd8aa6351e8664b4f996bd10fa3650c53
damarvijis <damarjipiz@gmail.com>
2024-06-25 09:05:10 +0700

    main:add staticpage component

---
+++ internal/base/components/StaticPage.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head"
+import { ReactElement, ReactNode } from "react"
+import { Inter } from "next/font/google"
+const inter = Inter({ subsets: ["latin"] })
+
+export type StaticPageProps = {
+  children: ReactNode
+  title: string
+}
+
+export const StaticPage = ({ title, children }: StaticPageProps): ReactElement => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="keywords" content="product list, product detail" />
+        <meta name="description" content="Product App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${inter.className}`}>{children}</main>
+    </>
+  )
+}
