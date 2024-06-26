577749e66f5598b02e6f012dcb2bca3aa3aabeac
damarvijis <damarjipiz@gmail.com>
2024-06-26 16:22:30 +0700

    main: remove storybook page & widget

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,13 @@
 import type { StorybookConfig } from "@storybook/nextjs"
 
 const config: StorybookConfig = {
-  stories: ["../internal/**/*.(stories|story).@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-essentials", "storybook-dark-mode", "@storybook/addon-styling-webpack"],
+  stories: ["../internal/**/*.mdx", "../internal/**/*.(stories|story).@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "storybook-dark-mode",
+    "@storybook/addon-styling-webpack",
+    "@storybook/addon-interactions",
+  ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
